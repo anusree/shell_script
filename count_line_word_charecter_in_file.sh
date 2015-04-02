c4ebbaa7f45885e421ad09fb9cd790f77a1bdf40
Anusree <anusree.a04@gmail.com>
2015-04-02 15:01:34 +0530

    count_line_word_charecter_in_file

--- count_line_word_charecter_in_file.sh
+++ count_line_word_charecter_in_file.sh
@@ -5,3 +5,10 @@ touch word_count.txt
 cat > word_count.txt 
 #to count the number of lines, words, charecter using the command wc
 wc word_count.txt
+#if you want to see only the number of lines then wc with the option -l
+wc -l word_count.txt
+#if you want to see only the number of words then wc with the option -w
+wc -w word_count.txt
+#if you want to see only the number of charecter then wc with the option -c
+wc -c word_count.txt
+
