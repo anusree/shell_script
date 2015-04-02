15927575cb9176ccc733dcaa43dfa4f3b8d9c41e
Anusree <anusree.a04@gmail.com>
2015-04-02 11:40:26 +0530

    create_directories_and_file

---
+++ create_directories_file.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#create directory using mkdir command:
+mkdir anu
+#go to the directoy that we have previously created
+cd anu
+#create empty file using touch command:
+touch anu.txt
+#adding content to this file open the fime using vim editor and you can write content
