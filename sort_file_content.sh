a83654defd1d4de2797337131d39c498c7a24fa5
Anusree <anusree.a04@gmail.com>
2015-04-02 15:11:08 +0530

    sort_file_content

---
+++ sort_file_content.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#first we have to create an empty file using touch command
+touch animals.txt
+#next, enter the content to the file
+cat > animal.txt
+#view the contet that we enter into the file 
+echo  The content are 
+cat animal.txt
+#sort the content that we enter into the file animal.txt using the command sort
+echo  The sorted content is 
+sort animal.txt
