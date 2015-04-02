0a542fe2ca985de819f5b5716a3cdf7845e5d1f3
Anusree <anusree.a04@gmail.com>
2015-04-02 12:51:15 +0530

    command_ls

---
+++ command_ls.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#to find the directories and files in the current directory using the command ls
+ls
+#diffrent options in ls command
+#long listing with permissions on thant files and directories
+ls -l
+#to find hidden files in the current directories
+ls -a
